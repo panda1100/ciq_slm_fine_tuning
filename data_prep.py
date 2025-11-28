a4650916166ed5eb4e9dc804e8dabac2788f3f0c
Yoshiaki Senda <yoshiaki@live.it>
2025-11-28 11:06:48 +0900

    added data_prep.py

---
+++ data_prep.py
@@ -0,0 +1,59 @@
+from unsloth import FastLanguageModel
+import torch
+from datasets import load_dataset
+from unsloth import to_sharegpt
+from unsloth import standardize_sharegpt
+from unsloth import apply_chat_template
+
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+
+# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
+fourbit_models = [
+    "unsloth/mistral-7b-v0.3-bnb-4bit",      # New Mistral v3 2x faster!
+    "unsloth/mistral-7b-instruct-v0.3-bnb-4bit",
+    "unsloth/llama-3-8b-bnb-4bit",           # Llama-3 15 trillion tokens model 2x faster!
+    "unsloth/llama-3-8b-Instruct-bnb-4bit",
+    "unsloth/llama-3-70b-bnb-4bit",
+    "unsloth/Phi-3-mini-4k-instruct",        # Phi-3 2x faster!
+    "unsloth/Phi-3-medium-4k-instruct",
+    "unsloth/mistral-7b-bnb-4bit",
+    "unsloth/gemma-7b-bnb-4bit",             # Gemma 2.2x faster!
+] # More models at https://huggingface.co/unsloth
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/llama-3-8b-bnb-4bit",
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+)
+
+dataset = load_dataset("vicgalle/alpaca-gpt4", split="train")
+
+dataset = to_sharegpt(
+    dataset,
+    merged_prompt="{instruction}[[\nYour input is:\n{input}]]",
+    output_column_name="output",
+    conversation_extension=3,  # Select more to handle longer conversations
+)
+
+dataset = standardize_sharegpt(dataset)
+
+chat_template = """Below are some instructions that describe some tasks. Write responses that appropriately complete each request.
+
+### Instruction:
+{INPUT}
+
+### Response:
+{OUTPUT}"""
+
+dataset = apply_chat_template(
+    dataset,
+    tokenizer=tokenizer,
+    chat_template=chat_template,
+    # default_system_message = "You are a helpful assistant", << [OPTIONAL]
+)
+
+dataset.save_to_disk("/scratch/dataset")
