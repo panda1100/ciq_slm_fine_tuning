6406e906f4af40abc6483068c1870fc58a2874cd
Yoshiaki Senda <yoshiaki@live.it>
2025-11-30 13:44:48 +0900

    workaround

--- fine_tuning.py
+++ fine_tuning.py
@@ -10,13 +10,21 @@ max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
 dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
 load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
 
+
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "model",
+    model_name = "unsloth/llama-3-8b-bnb-4bit",
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = load_in_4bit,
     # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
 )
+#model, tokenizer = FastLanguageModel.from_pretrained(
+#    model_name = "model",
+#    max_seq_length = max_seq_length,
+#    dtype = dtype,
+#    load_in_4bit = load_in_4bit,
+#    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+#)
 
 model = FastLanguageModel.get_peft_model(
     model,
@@ -65,7 +73,7 @@ import traceback
 
 try:
     model.save_pretrained_merged(
-        "model",
+        "finetuned",
         tokenizer,
         save_method="merged_16bit",
     )
